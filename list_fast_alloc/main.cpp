4480a1ddb81e4fd47726b064904a127e5653544b
Orlov Aleksey <orlov.aleksei53@gmail.com>
2023-06-19 15:34:26 +0300

    added files list

---
+++ list_fast_alloc/main.cpp
@@ -0,0 +1,255 @@
+#include <iostream>
+#include "fastallocator.h"
+#include <vector>
+#include <list>
+#include <time.h>
+#include <assert.h>
+#include "test.cpp"
+#include <type_traits>
+#include <string>
+#include <utility>
+#include <iomanip>
+
+using namespace std;
+/*
+struct S {
+    static int ctor_calls;
+    static int dtor_calls;
+    int a, b;
+    S() {
+        ++ctor_calls;
+
+        a = 5;
+        b = 10;
+    }
+    S(const S& s1) : a(s1.a), b(s1.b) {
+        ++ctor_calls;
+
+    }
+    ~S() {
+        dtor_calls++;
+    }
+};
+int S::ctor_calls = 0;
+int S::dtor_calls = 0;
+
+struct D {
+    char a, b, c;
+    D() {
+        a = 'a';
+        b = 'b';
+        c = 'c';
+    }
+};
+ */
+
+int main() {
+/*
+    FixedAllocator<8> alloc;
+    int* a = reinterpret_cast<int*>(alloc.allocate(1));
+    for (int i = 0; i < 8; ++i) {
+        cin >> a[i];
+    }
+
+    for (int i = 0; i < 8; ++i) {
+        cout << a[i];
+    }
+    alloc.deallocate(reinterpret_cast<int8_t *>(a));
+*/
+/*
+    vector<int, FastAllocator<int>> v(4);
+    for (int i = 0; i < 4; ++i) {
+        cin >> v[i];
+    }
+
+    for (int i = 0; i < 4; ++i) {
+        cout << v[i];
+    }
+    v.pop_back();
+*/
+/*
+    std::list<int> original;
+    std::list<int, FastAllocator<int>> fast;
+
+
+    clock_t start1 = clock();
+    for (int i = 0; i < 100000; ++i) {
+        original.push_back(i);
+    }
+    for (int i = 0; i < 100000; ++i) {
+        original.pop_back();
+    }
+    clock_t end1 = clock();
+    double seconds1 = (double)(end1 - start1) / CLOCKS_PER_SEC;
+    cout << "original: "  << seconds1 << '\n';
+
+
+    clock_t start2 = clock();
+    for (int i = 0; i < 10; ++i) {
+        fast.push_back(i);
+    }
+    for (int i = 0; i < 100000; ++i) {
+        fast.pop_back();
+    }
+    clock_t end2 = clock();
+    double seconds2 = (double)(end2 - start2) / CLOCKS_PER_SEC;
+    cout << "fast: "  << seconds2 << '\n';
+*/
+
+    vector<int, FastAllocator<int>> v;
+    list<int, FastAllocator<int>> orig1(10);
+    list<int, FastAllocator<int>> orig2(1);
+    orig2 = orig1;
+    orig2.push_back(7);
+    orig2.push_front(4);
+    deque<int, FastAllocator<int>> d;
+//    for (auto i : orig2) {
+//        cout << i;
+//    }
+    for ( list<int, FastAllocator<int>>::iterator it = orig2.begin(); it != orig2.end(); ++it ) {
+        int x = *it;
+        cout << x;
+    }
+
+/*
+    List<int, FastAllocator<int>> lst1(10);
+    lst1.push_back(2);
+    lst1.push_front(3);
+    List<int, FastAllocator<int>> lst2;
+    lst2 = lst1;
+    lst2.pop_back();
+    lst2.pop_front();
+    lst2.insert(++lst2.begin(), 8);
+    lst2.erase(++lst2.begin());
+    lst1.print();
+    lst2.print();
+    */
+
+/*
+    List<int, FastAllocator<int>> lst;
+    for (int i = 0; i < 10; ++i) {
+        lst.push_back(i);
+    }
+    List<int, FastAllocator<int>>::iterator normal = lst.begin();
+    List<int, FastAllocator<int>>::const_iterator constant = lst.begin();
+    if (constant == normal) {
+        cout << "good" << '\n';
+    }
+//    normal = constant;
+    constant = normal;
+
+    auto itera = lst.end();
+    lst.insert(itera, 1);
+    --itera;
+    lst.erase(itera);
+
+    auto it = lst.begin();
+    while (it != lst.end()) {
+        cout << *it;
+        ++it;
+    }
+    */
+/*
+    List<int, FastAllocator<int>> lst(10, 1);
+    for (int i = 0; i < 100000; ++i) {
+        lst.push_back(i);
+    }
+    for (int i = 0; i < 100000; ++i) {
+        lst.pop_back();
+    }
+    assert(lst.size() == 10);
+    auto it = lst.rbegin();
+    while (it != lst.rend()) {
+        cout << *it;
+        ++it;
+    }
+    */
+/*
+    {
+        List<S, FastAllocator<S>> lst(5);
+    }
+    cout << S::ctor_calls << '\n';
+    cout << S::dtor_calls << '\n';
+*/
+/*
+    List<int, FastAllocator<int>> lst(10);
+    for (int i = 0; i < 10000; ++i) {
+        lst.push_back(i);
+    }
+//    for (int i = 0; i < 1000000; ++i) {
+//        lst.pop_back();
+//    }
+    auto it = lst.end(); it--;
+    auto end = lst.begin(); end++; end++; end++; end++;
+    while (end != it) {
+        lst.erase(it);
+        it--;
+    }
+    for (auto i : lst) {
+        cout << i << " ";
+    }
+    */
+
+/*
+    FastAllocator<D> fast1;
+    FastAllocator<D> fast2;
+    fast2 = fast1;
+
+    List<int, FastAllocator<int>> lst1;
+    List<int, FastAllocator<int>> lst2;
+    lst2 = lst1;
+*/
+
+//    List<int, FastAllocator<int>> fast;
+//
+//    test_list<List<int, FastAllocator<int>>>(std::move(fast));
+//    std::deque<char, FastAllocator<char>> deq;
+//
+//    test_std_deque(std::move(deq));
+//    BasicListTest<>();
+
+//    BasicListTest<FastAllocator<int>>();
+
+//    TestAccountant<>(); //TROUBLES
+
+
+
+//    TestAccountant<FastAllocator<Accountant>>();
+
+//    TestNotDefaultConstructible<>();
+//    TestNotDefaultConstructible<FastAllocator<NotDefaultConstructible>>();
+
+//    TestNotDefaultConstructible<FastAllocator<deque<int>>>();
+
+//    deque<char, FastAllocator<char>> deq1;
+//    for (int i = 0; i < 10000; ++i) {
+//        deq1.push_back('a');
+//        deq1.push_front('a');
+//    }
+//    for (int i = 0; i < 10000; ++i) {
+//        deq1.pop_back();
+//        deq1.pop_front();
+//    }
+//    deque<char, FastAllocator<char>> deq2 = std::move(deq1);
+
+
+//    test_std_deque(std::deque<char, FastAllocator<char>>());
+//    TestWhimsicalAllocator();
+
+//    TestWhimsicalAllocator();
+
+//    TestPerformance<std::list>();
+
+    BasicListTest<FastAllocator<int>>();
+
+//    lst.print();
+//    for (auto i : lst2) {
+//        cout << i;
+//    }
+//    List<int, FastAllocator<int>>::reverse_iterator it = lst2.rbegin();
+//    while(it != lst2.rend()) {
+//        cout << *it;
+//        it++;
+//    }
+    return 0;
+}
---
+++ list_fast_alloc/test.cpp
@@ -0,0 +1,410 @@
+#pragma once
+#include <stdexcept>
+#include <string>
+#include <list>
+#include <vector>
+#include <deque>
+#include <memory>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <sstream>
+#include <cassert>
+#include <chrono>
+
+//template<typename T, typename Alloc = std::allocator<T>>
+//using List = std::list<T, Alloc>;
+
+//template<typename T>
+//using FastAllocator = std::allocator<T>;
+
+template <typename Alloc = std::allocator<int>>
+void BasicListTest() {
+    List<int, Alloc> lst;
+
+    assert(lst.size() == 0);
+
+    lst.push_back(3);
+    lst.push_back(4);
+    lst.push_front(2);
+    lst.push_back(5);
+    lst.push_front(1);
+
+    std::reverse(lst.begin(), lst.end());
+    // now lst is 5 4 3 2 1
+
+    assert(lst.size() == 5);
+
+    std::string s;
+    for (int x: lst) {
+        s += std::to_string(x);
+    }
+    assert(s == "54321");
+    //std::cerr << "Tests log: check 1.1 ok, list contains 5 4 3 2 1" << std::endl;
+
+    auto cit = lst.cbegin();
+    std::advance(cit, 3);
+
+    lst.insert(cit, 6);
+    lst.insert(cit, 7);
+
+    std::advance(cit, -3);
+    lst.insert(cit, 8);
+    lst.insert(cit, 9);
+    // now lst is 5 4 8 9 3 6 7 2 1
+
+    assert(lst.size() == 9);
+
+    s.clear();
+    for (int x: lst) {
+        s += std::to_string(x);
+    }
+    assert(s == "548936721");
+    //std::cerr << "Tests log: check 1.2 ok, list contains 5 4 8 9 3 6 7 2 1" << std::endl;
+
+    lst.erase(lst.cbegin());
+    lst.erase(cit);
+
+    lst.pop_front();
+    lst.pop_back();
+
+    const auto copy = lst;
+    assert(lst.size() == 5);
+    assert(copy.size() == 5);
+    // now both lists are 8 9 6 7 2
+
+    s.clear();
+    for (int x: lst) {
+        s += std::to_string(x);
+    }
+    assert(s == "89672");
+    //std::cerr << "Tests log: check 1.3 ok, list contains 8 9 6 7 2" << std::endl;
+
+    auto rit = lst.rbegin();
+    ++rit;
+    lst.erase(rit.base());
+    assert(lst.size() == 4);
+
+    rit = lst.rbegin();
+    *rit = 3;
+
+    // now lst: 8 9 6 3, copy: 8 9 6 7 2
+    s.clear();
+    s.clear();
+    for (int x: lst) {
+        s += std::to_string(x);
+    }
+    assert(s == "8963");
+
+    assert(copy.size() == 5);
+
+    s.clear();
+    for (int x: copy) {
+        s += std::to_string(x);
+    }
+    assert(s == "89672");
+
+    //std::cerr << "Tests log: check 1.4 ok, list contains 8 9 6 3, another list is still 8 9 6 7 2" << std::endl;
+
+    typename List<int, Alloc>::const_reverse_iterator crit = rit;
+    crit = copy.rbegin();
+    assert(*crit == 2);
+
+    cit = crit.base();
+    std::advance(cit, -2);
+    assert(*cit == 7);
+
+}
+
+struct VerySpecialType {
+    int x = 0;
+    explicit VerySpecialType(int x): x(x) {}
+};
+
+struct NotDefaultConstructible {
+    NotDefaultConstructible() = delete;
+    NotDefaultConstructible(VerySpecialType x): x(x) {}
+    VerySpecialType x;
+};
+
+template <class List>
+int test_list(List&& l) {
+    using namespace std::chrono;
+
+    std::ostringstream oss;
+    std::cout << "still running1" << std::endl;
+
+    auto start = high_resolution_clock::now();
+
+    for (int i = 0; i < 1'000'000; ++i) {
+        l.push_back(i);
+    }
+    std::cout << "still running2" << std::endl;
+
+    auto it = l.begin();
+    for (int i = 0; i < 1'000'000; ++i) {
+        l.push_front(i);
+    }
+    oss << *it;
+    std::cout << "still running3" << std::endl;
+
+    auto it2 = std::prev(it);
+    for (int i = 0; i < 2'000'000; ++i) {
+        l.insert(it, i);
+        if (i % 534'555 == 0) {
+            oss << *it;
+        }
+    }
+    oss << *it;
+    std::cout << "still running4" << std::endl;
+    for (int i = 0; i < 1'500'000; ++i) {
+        l.pop_back();
+        if (i % 342'985 == 0) oss << *l.rbegin();
+    }
+    oss << *l.rbegin();
+    std::cout << "still running5" << std::endl;
+
+    for (int i = 0; i < 1'000'000; ++i) {
+        l.erase(it2++);
+        if (i % 432'098 == 0) oss << *it2;
+    }
+    oss << *it2;
+    std::cout << "still running6" << std::endl;
+
+    for (int i = 0; i < 1'000'000; ++i) {
+        l.pop_front();
+    }
+    oss << *l.begin();
+    std::cout << "still running7" << std::endl;
+
+    for (int i = 0; i < 1'000'000; ++i) {
+        l.push_back(i);
+    }
+    oss << *l.rbegin();
+    std::cout << "still running8" << std::endl;
+
+    assert(oss.str() == "00000099999865701331402819710431628058149999904320988641969999991000000999999");
+
+    auto finish = high_resolution_clock::now();
+    return duration_cast<milliseconds>(finish - start).count();
+}
+
+void test_std_deque(std::deque<char, FastAllocator<char>>&& d) {
+    d.push_back(1);
+    assert(d.back() == 1);
+
+    d.resize(2'500'000, 5);
+    assert(d[1'000'000] == 5);
+
+    d.pop_back();
+    for (int i = 0; i < 2'000'000; ++i) {
+        d.push_back(i % 100);
+    }
+
+    assert(d.size() == 4'499'999);
+    //std::cerr << (int)d[4'000'000] << std::endl;
+    assert(d[4'000'000] == 1);
+
+    for (int i = 0; i < 4'000'000; ++i) {
+        d.pop_front();
+    }
+
+    assert(d[400'000] == 1);
+}
+struct Accountant {
+    // Some field of strange size
+    char arr[40];
+
+    static size_t ctor_calls;
+    static size_t dtor_calls;
+
+    Accountant() {
+        ++ctor_calls;
+    }
+    Accountant(const Accountant&) {
+        ++ctor_calls;
+    }
+
+    Accountant& operator=(const Accountant&) {
+        ++ctor_calls;
+        ++dtor_calls;
+        return *this;
+    }
+
+    Accountant(Accountant&&) = delete;
+    Accountant& operator=(Accountant&&) = delete;
+
+    ~Accountant() {
+        ++dtor_calls;
+    }
+};
+
+size_t Accountant::ctor_calls = 0;
+size_t Accountant::dtor_calls = 0;
+
+template <typename Alloc = std::allocator<NotDefaultConstructible>>
+void TestNotDefaultConstructible() {
+    List<NotDefaultConstructible, Alloc> lst;
+    assert(lst.size() == 0);
+    lst.push_back(VerySpecialType(0));
+    assert(lst.size() == 1);
+    lst.pop_front();
+    assert(lst.size() == 0);
+}
+
+template<typename Alloc = std::allocator<Accountant>>
+void TestAccountant() {
+    Accountant::ctor_calls = 0;
+    Accountant::dtor_calls = 0;
+
+    {
+        List<Accountant, Alloc> lst(5);
+        assert(lst.size() == 5);
+        assert(Accountant::ctor_calls == 5);
+
+        List<Accountant, Alloc> another = lst;
+        assert(another.size() == 5);
+        assert(Accountant::ctor_calls == 10);
+        assert(Accountant::dtor_calls == 0);
+
+        another.pop_back();
+        another.pop_front();
+        assert(Accountant::dtor_calls == 2);
+
+        lst = another; // dtor_calls += 5, ctor_calls += 3
+        assert(another.size() == 3);
+        assert(lst.size() == 3);
+
+        assert(Accountant::ctor_calls == 13);
+        assert(Accountant::dtor_calls == 7);
+
+    } // dtor_calls += 6
+
+    assert(Accountant::ctor_calls == 13);
+    assert(Accountant::dtor_calls == 13);
+}
+
+template <typename T, bool PropagateOnConstruct, bool PropagateOnAssign>
+struct WhimsicalAllocator : public std::allocator<T> {
+    std::shared_ptr<int> number;
+
+    auto select_on_container_copy_construction() const {
+        return PropagateOnConstruct
+               ? WhimsicalAllocator<T, PropagateOnConstruct, PropagateOnAssign>()
+               : *this;
+    }
+
+    struct propagate_on_container_copy_assignment
+            : std::conditional_t<PropagateOnAssign, std::true_type, std::false_type>
+    {};
+
+    template <typename U>
+    struct rebind {
+        using other = WhimsicalAllocator<U, PropagateOnConstruct, PropagateOnAssign>;
+    };
+
+    WhimsicalAllocator(): number(std::make_shared<int>(counter)) {
+        ++counter;
+    }
+
+    template <typename U>
+    WhimsicalAllocator(const WhimsicalAllocator<U, PropagateOnConstruct, PropagateOnAssign>& another)
+            : number(another.number)
+    {}
+
+    template <typename U>
+    auto& operator=(const WhimsicalAllocator<U, PropagateOnConstruct, PropagateOnAssign>& another) {
+        number = another.number;
+        return *this;
+    }
+
+    template <typename U>
+    bool operator==(const WhimsicalAllocator<U, PropagateOnConstruct, PropagateOnAssign>& another) const {
+        return *number == *another.number;
+    }
+
+    template <typename U>
+    bool operator!=(const WhimsicalAllocator<U, PropagateOnConstruct, PropagateOnAssign>& another) const {
+        return *number != *another.number;
+    }
+
+    static size_t counter;
+};
+
+template <typename T, bool PropagateOnConstruct, bool PropagateOnAssign>
+size_t WhimsicalAllocator<T, PropagateOnConstruct, PropagateOnAssign>::counter = 0;
+
+void TestWhimsicalAllocator() {
+    {
+        List<int, WhimsicalAllocator<int, true, true>> lst;
+
+        lst.push_back(1);
+        lst.push_back(2);
+
+        auto copy = lst;
+        assert(copy.get_allocator() != lst.get_allocator());
+
+        lst = copy;
+        assert(copy.get_allocator() == lst.get_allocator());
+    }
+    {
+        List<int, WhimsicalAllocator<int, false, false>> lst;
+
+        lst.push_back(1);
+        lst.push_back(2);
+
+        auto copy = lst;
+        assert(copy.get_allocator() == lst.get_allocator());
+
+        lst = copy;
+        assert(copy.get_allocator() == lst.get_allocator());
+    }
+    {
+        List<int, WhimsicalAllocator<int, true, false>> lst;
+
+        lst.push_back(1);
+        lst.push_back(2);
+
+        auto copy = lst;
+        assert(copy.get_allocator() != lst.get_allocator());
+
+        lst = copy;
+        assert(copy.get_allocator() != lst.get_allocator());
+    }
+}
+
+
+template <template<typename, typename> class Container>
+void TestPerformance() {
+    std::ostringstream oss_first;
+    std::ostringstream oss_second;
+    int first = test_list(Container<int, std::allocator<int>>());
+    int second = test_list(Container<int, FastAllocator<int>>());
+    std::ignore = first;
+    std::ignore = second;
+    first = 0, second = 0;
+    double mean_first = 0.0;
+    double mean_second = 0.0;
+
+    for (int i = 0; i < 5; ++i) {
+        first = test_list(Container<int, std::allocator<int>>());
+        mean_first += first;
+        oss_first << first << " ";
+
+        second = test_list(Container<int, FastAllocator<int>>());
+        mean_second += second;
+        oss_second << second << " ";
+    }
+
+    mean_first /= 5;
+    mean_second /= 5;
+
+    std::cerr << "Tests log: Results with std::allocator: " << oss_first.str()
+              << " ms, results with FastAllocator: " << oss_second.str() << " ms " << std::endl;
+
+    if (mean_first * 0.9 < mean_second) {
+        throw std::runtime_error("FastAllocator expected to be at least 10\% faster than std::allocator, but mean time were "
+                                 + std::to_string(mean_second) + " ms comparing with " + std::to_string(mean_first) + " :((( ...\n");
+    }
+}
+
